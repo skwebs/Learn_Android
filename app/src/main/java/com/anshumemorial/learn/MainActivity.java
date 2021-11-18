f03616331a4bcf9178ad257d6c5c7f223acaad1c
Satish Kumar Sharma <skwebserv@gmail.com>
2021-11-18 09:44:48 +0530

    Added webview in activity2 and change api domain from anshumemorial.in to naitaknik.com

--- app/src/main/java/com/anshumemorial/learn/MainActivity.java
+++ app/src/main/java/com/anshumemorial/learn/MainActivity.java
@@ -1,6 +1,5 @@
 package com.anshumemorial.learn;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -24,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         Button getData = findViewById(R.id.get_data);
         Button goToSecondPage = findViewById(R.id.go_to_second_page);
         Button showToastBtn = findViewById(R.id.show_toast_btn);
+        Button goToRegPage = findViewById(R.id.go_to_reg_page);
 
         getData.setOnClickListener(v -> getData());
 
@@ -32,31 +32,31 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        goToRegPage.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
         showToastBtn.setOnClickListener(v -> Toast.makeText(this,"Welcome in First Page", Toast.LENGTH_LONG).show());
-//                Toast
-        Context context = getApplicationContext();
-        CharSequence text = getString(R.string.welcome_to_page_1);
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast toast = Toast.makeText(context, text, duration);
+//      Toast
+        Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.welcome_to_page_1), Toast.LENGTH_LONG);
         toast.show();
-
-
     }
 
     private void getData() {
-        String url ="https://anshumemorial.in/api/api/v1/user/2";
+        String url ="https://naitaknik.com/ci4_ama_api/api/v1/student/1";
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, response -> {
                     try {
-                        CharSequence res = response.getInt("id") + "\n" + response.getString("name") + "\n" + response.getString("email");
+                        CharSequence res = response.getInt("id") + "\n" + response.getString("first_name") + "\n" + response.getString("email");
                         Toast.makeText(this, res, Toast.LENGTH_LONG).show();
 
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
+
                 }, System.out::print);
 
         requestQueue.add(jsonObjectRequest);
--- app/src/main/java/com/anshumemorial/learn/MainActivity2.java
+++ app/src/main/java/com/anshumemorial/learn/MainActivity2.java
@@ -1,36 +1,110 @@
 package com.anshumemorial.learn;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Button Btn1 = findViewById(R.id.go_to_second_page);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new myWebViewClient());
+        webView.setWebChromeClient(new myWebChromeClient());
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAppCacheEnabled(true);
+//         Improve loading speed
+        settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        settings.setUseWideViewPort(true);
+        settings.setSaveFormData(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.loadUrl("https://anshumemorial.in");
+    }
 
-        Btn1.setOnClickListener(v -> {
+    @Override
+    public void onBackPressed(){
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-            Intent intent = new Intent(MainActivity2.this, MainActivity.class);
-            startActivity(intent);
-        });
+    private class myWebViewClient extends WebViewClient {
 
-        //            Toast
-        Context context = getApplicationContext();
-        CharSequence text = getString(R.string.welcome_to_page_2);
-        int duration = Toast.LENGTH_SHORT;
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.startsWith("mailto:")) {
+                startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse(url)));
+            } else if(url.startsWith("tel:") || url.startsWith("whatsapp:")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        }
 
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            final Uri uri = request.getUrl();
+            if (uri.toString().startsWith("mailto:")) {
+                //Handle mail Urls
+                startActivity(new Intent(Intent.ACTION_SENDTO, uri));
+            } else if (uri.toString().startsWith("tel:")) {
+                //Handle telephony Urls
+                startActivity(new Intent(Intent.ACTION_DIAL, uri));
+            } else if (uri.toString().startsWith("whatsapp:")) {
+                //Handle whatsapp Urls
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            } else {
+                //Handle Web Urls
+                view.loadUrl(uri.toString());
+            }
+            return true;
+        }
 
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            super.onReceivedError(view, errorCode, description, failingUrl);
+        }
 
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+        }
+    }
+
+    private static class myWebChromeClient extends WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+        }
     }
 }
